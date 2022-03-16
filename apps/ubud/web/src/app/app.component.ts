b7a16ae3a16d5b49a6d60fb09508feb9048ad104
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-03-16 20:32:13 +0700

    loop cards instead of manual copy paste

--- apps/ubud/web/src/app/app.component.ts
+++ apps/ubud/web/src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'ubud-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'], // opsional
+  selector: 'ubud-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'], // opsional
 })
 export class AppComponent {
+  @Input() public cards = [
+    {
+      title: 'Lorem Ipsum',
+      value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, totam sint.',
+    },
+    {
+      title: 'Lorem Ipsum',
+      value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, totam sint.',
+    },
+  ];
 }
