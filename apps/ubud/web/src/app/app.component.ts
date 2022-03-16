2408cdb97d169616fb6250b0dc563a44b4dd204a
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2022-03-16 20:34:36 +0700

    change short lorem ipsum to one paragraph to show truncate

--- apps/ubud/web/src/app/app.component.ts
+++ apps/ubud/web/src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   @Input() public cards = [
     {
       title: 'Lorem Ipsum',
-      value: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, totam sint.',
+      value: 'Duis laboris nostrud reprehenderit et est. Ex commodo fugiat laboris cupidatat occaecat in deserunt. Minim in ex consectetur voluptate ad excepteur sunt duis id laborum consequat non excepteur. Elit consectetur consequat duis adipisicing duis eiusmod eiusmod reprehenderit nisi laborum Lorem nulla. Est consectetur veniam adipisicing culpa qui id sint sunt dolor ullamco enim velit labore. Fugiat laborum culpa eiusmod adipisicing ad Lorem incididunt dolor dolore nostrud consectetur. Ad minim pariatur et enim.',
     },
     {
       title: 'Lorem Ipsum',
