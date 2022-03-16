1c75f2b37e261a9c04bdc18debc3c530e207a11e
Julius Kayne <juliuskayne07@gmail.com>
2022-03-16 22:51:56 +0700

    add-content: sidebar, navbar, truncate pipe

--- libs/element/common/src/index.ts
+++ libs/element/common/src/index.ts
@@ -3,3 +3,9 @@ export * from './lib/modules/components/cards/shadow/module';
 
 /* --- Pipes --- */
 export * from './lib/modules/pipes/module';
+
+/* --- Navbar --- */
+export * from './lib/modules/components/navigation/module';
+
+// sidebar
+export * from './lib/modules/components/sidebar/module';
