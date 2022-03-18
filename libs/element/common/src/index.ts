ea86390798bdb7f430a92bb2c00c5ef8d00cf065
Erick Chandra <erickchandra718@gmail.com>
2022-03-18 21:15:32 +0700

    add: navbar

--- libs/element/common/src/index.ts
+++ libs/element/common/src/index.ts
@@ -1,5 +1,6 @@
 /* --- Components --- */
 export * from './lib/modules/components/cards/shadow/module';
+export * from './lib/modules/components/navbar/module';
 
 /* --- Pipes --- */
 export * from './lib/modules/pipes/module';
