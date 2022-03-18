4b968fae828e66681066e9da40933559272d1c8f
Farrell Eduardo <farrell.6659@ski.sch.id>
2022-03-19 00:15:02 +0700

    Angular
    - navbar, sidebar, card content

---
+++ libs/element/common/src/lib/modules/components/cardcomponent/cardcomponent.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component, NgModule } from "@angular/core";
+
+@Component({
+    selector: "ubud-element-common-card-component-component",
+    templateUrl: './cardcomponent.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class CardComponent{
+
+}
---
+++ libs/element/common/src/lib/modules/components/navbar/module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { Navbar } from "./navbar";
+
+@NgModule({
+    declarations: [
+        Navbar,
+    ],
+    imports: [
+        CommonModule,
+    ],
+    exports: [
+        Navbar,
+    ]
+})
+
+export class UbudElementCommonNavbarComponentModule {}
